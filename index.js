153d2bf442313e0649059b668138ecaa03f03881
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-02 13:01:34 -0800

    Access db with .find function. Create .then and .catch functions with respective responses that match project requirements.

--- index.js
+++ index.js
@@ -8,8 +8,18 @@ server.use(express.json());
 const db = require('./data/db');
 
 server.get('/api/users', (req, res) => {
-db.find().then().catch()
-})
+    db.find()
+        .then(data => {
+            res.status(200).json(data)
+        })
+        .catch(error => {
+            console.log('error on GET /api/users', error)
+            res
+                .end()
+                .status(500)
+                .json({ error: "The users information could not be retrieved." })
+        });
+});
 
 
 
@@ -18,6 +28,6 @@ db.find().then().catch()
 
 const port = 5000
 
-server.listen(port, () => 
+server.listen(port, () =>
     console.log(`API is running on ${port}`)
 );
