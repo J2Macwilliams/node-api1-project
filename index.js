b803516c5e5952e33236f2290fb197a0f91554e8
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-02 13:48:38 -0800

    Create .post function with respective responses. Access db with .insert method.

--- index.js
+++ index.js
@@ -7,6 +7,29 @@ server.use(express.json());
 
 const db = require('./data/db');
 
+server.post('/api/users', (req, res) => {
+    const dbInfo = req.body;
+
+    db.insert(dbInfo)
+        .then(info => {
+            res.status(201).json(info)
+        })
+        .catch(error => {
+            console.log('error on the POST /api/users', error)
+            if (dbInfo !== "name" && "bio") {
+                res
+                    .end()
+                    .status(400)
+                    .json({ error: "Please provide name and bio for the user." })
+            } else {
+                res
+                    .end()
+                    .status(500)
+                    .json({ error: "There was an error while saving the user to the database" })
+            }
+        });
+})
+
 server.get('/api/users', (req, res) => {
     db.find()
         .then(data => {
