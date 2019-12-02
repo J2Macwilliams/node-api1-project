016af87993ced1645c1dfa19988c5ba77b0585a9
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-02 12:43:10 -0800

    Create port 5000. Create server listener function with console.log to verify.

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ const express = require('express');
 const server = express();
 
 server.use(express.json());
+
+
+
+
+
+
+
+
+const port = 5000
+
+server.listen(port, () => 
+    console.log(`API is running on ${port}`)
+);
